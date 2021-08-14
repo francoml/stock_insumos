6c2cf13e7a131aaca048e76978a21ec9e8706bda
francoml <francoml92@gmail.com>
2021-08-14 11:02:45 -0300

    jeje

--- layouts/header.php
+++ layouts/header.php
@@ -16,15 +16,15 @@
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker3.min.css" />
     <link rel="stylesheet" href="libs/css/main.css" /> -->
 
-  <!-- Latest compiled and minified CSS -->
+  <!-- Latest compiled and minified CSS 
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
 
-  <!-- Optional theme -->
+  <!-- Optional theme 
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
 
   <link rel="stylesheet" href="libs/bootstrap-datepicker/css/bootstrap-datepicker.min.css" />
 
-  <link rel="stylesheet" href="libs/css/main.css" />
+  <link rel="stylesheet" href="libs/css/main.css" /> -->
 
 </head>
 
