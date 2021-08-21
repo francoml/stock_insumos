7d4df55ea33cf749610bd8540ad89d61afb7a43c
francoml <francoml92@gmail.com>
2021-08-21 18:55:06 -0300

    cambios a forms de ingreso y salida

--- add_salida.php
+++ add_salida.php
@@ -1,23 +1,22 @@
 <?php
 ob_start();
-  $page_title = 'Nueva Salida';
-  require_once('includes/load.php');
-  // Checkin What level user has permission to view this page
-  page_require_level(2);
-  $all_productos = find_allASC('productos');
-  $all_destinatarios = find_all('destinatarios');
-  date_default_timezone_set('America/Argentina/Cordoba');
-  $usuario = getIniciales();
+$page_title = 'Nueva Salida';
+require_once('includes/load.php');
+// Checkin What level user has permission to view this page
+page_require_level(2);
+$all_productos = find_allASC('productos');
+$all_destinatarios = find_all('destinatarios');
+date_default_timezone_set('America/Argentina/Cordoba');
+$usuario = getIniciales();
 
 ?>
 
 <?php include_once('layouts/header.php'); ?>
 
-	<div class="row">
-	<div class="col-md-12">
-    <?php echo display_msg($msg); ?>
-	</div>
-	</div>
+<div class="container-fluid col-9">
+	<?php echo display_msg($msg); ?>
+</div>
+
 
 <form method="post" action="add_salida2.php" class="clearfix">
 
@@ -28,7 +27,7 @@ ob_start();
 			<h2>Nueva Salida</h2>
 		</div>
 
-		<div class="row justify-content-start">
+		<div class="row">
 			<div class="col-md-2">
 				<br>
 				<label for="inputUser" class="form-label">Usuario</label>
@@ -39,59 +38,63 @@ ob_start();
 				<label for="inputdate" class="form-label">Fecha de Salida</label>
 				<input readonly="readonly" type="text" class="form-control shadow-sm" autocomplete="off" value="<?php echo date("d/m/Y"); ?>">
 			</div>
-		</div>
-
-		<br>
-		<div class="row justify-content-start">
-			<div class="col-md-12">
+			<div class="col-md-4">
+				<br>
 				<label for="inputdate" class="form-label">Destinatario</label>
 				<select class="form-control shadow-sm" name="egreso-destinatario" tabindex="1">
 					<option value="">Seleccione Destino</option>
-					<?php  foreach ($all_destinatarios as $dest): ?>
-					<option value="<?php echo (int)$dest['id'] ?>"> 
-					<?php echo $dest['nombredest'] ?></option> va a buscar el destinatario por su id, pero va a mostrar su nombre
+					<?php foreach ($all_destinatarios as $dest) : ?>
+						<option value="<?php echo (int)$dest['id'] ?>">
+							<?php echo $dest['nombredest'] ?></option> <!-- va a buscar el destinatario por su id, pero va a mostrar su nombre -->
 					<?php endforeach; ?>
 				</select>
 			</div>
 		</div>
+
 		<br>
+
 		<div class="row justify-content-start">
-			<div class="col-md-12">
+			<div class="col-md-6">
 				<label for="inputdate" class="form-label">Producto</label>
 				<select class="form-control" name="egreso-producto" tabindex="2">
 					<option value="">Seleccione Producto</option>
-					<?php  foreach ($all_productos as $prod): ?>
-					<option value="<?php echo (int)$prod['id'] ?>"> 
-					<?php echo $prod['name'] ?></option> va a buscar el producto por su id, pero va a mostrar su nombre
+					<?php foreach ($all_productos as $prod) : ?>
+						<option value="<?php echo (int)$prod['id'] ?>">
+							<?php echo $prod['name'] ?></option> va a buscar el producto por su id, pero va a mostrar su nombre
 					<?php endforeach; ?>
 				</select>
 			</div>
+			<div class="col-md-2">
+				<label for="inputdate" class="form-label">Cantidad</label>
+				<input type="number" min="1" class="form-control" autocomplete="off" name="egreso-cantidad" placeholder="" tabindex="3">
+			</div>
 		</div>
 		<br>
 		<div class="row justify-content-between">
-			<div class="col-md-2">
-			<label for="inputdate" class="form-label">Cantidad</label>
-				<input type="number" min= "1" class="form-control" autocomplete="off" name="egreso-cantidad" placeholder="" tabindex="3">			
-			</div>
+
 			<div class="col-md-6">
 				<label for="inputdate" class="form-label">Observaciones</label>
-				<textarea class="form-control shadow-sm" name="ingreso-observacion" rows="3" tabindex=""></textarea>
+				<textarea class="form-control shadow-sm" name="ingreso-observacion" rows="3" tabindex="4"></textarea>
 			</div>
 		</div>
+
 		<br><br>
-		<div class="row justify-content-between">
-			<div class="col-md-4">
-			<button type="submit" name="add_salida" class="btn btn-dark" tabindex="8" <?php $bandera=1; ?>> AGREGAR SALIDA</button>
+
+		<div class="row justify-content-end">
+			<div class="col-md-2">
+				<a href="home.php" class="btn btn-dark hoverDeny" tabindex=""><i class="bi bi-x-square"></i> CANCELAR</a>
 			</div>
-			<div class="col-md-6">
-				<a href="home.php" title="cancelar" class="btn btn-dark" tabindex="">CANCELAR</a>
+			<br> <br>
+			<div class="col-md-3">
+				<button type="submit" name="add_salida" class="btn btn-dark hoverAccept" tabindex="5"><i class="bi bi-plus-square"></i> CARGAR SALIDA</button>
 			</div>
 		</div>
 		<br>
+
 	</div>
 </form>
 
-		<!--<div class="row">
+<!--<div class="row">
 		<div class="col-md-10 box">
 		<div class="panel panel-default">
         <div class="panel-heading">
@@ -109,14 +112,14 @@ ob_start();
 			<div class="col-md-3">
 			<div class="input-group">
 				<span class="input-group-addon">Usuario</span>
-				<input readonly="readonly" type="text" class="form-control" style="text-align:center;" value="<?php echo $usuario['iniciales'];?>">
+				<input readonly="readonly" type="text" class="form-control" style="text-align:center;" value="<?php echo $usuario['iniciales']; ?>">
 			</div>
 			</div>
 			
 			<div class="col-md-4">
             <div class="input-group">
                 <span class="input-group-addon">Fecha de Egreso</span>
-                <input readonly="readonly" type="text" class="form-control" autocomplete="off"  style="text-align:center;" value="<?php echo date("d/m/Y");?>">
+                <input readonly="readonly" type="text" class="form-control" autocomplete="off"  style="text-align:center;" value="<?php echo date("d/m/Y"); ?>">
             </div>
             </div>
 
@@ -129,7 +132,7 @@ ob_start();
 			<div class="col-md-6">
 			<select class="form-control" name="egreso-destinatario" tabindex="1">
 			<option value="">Seleccione Destino</option>
-			<?php  foreach ($all_destinatarios as $dest): ?>
+			<?php foreach ($all_destinatarios as $dest) : ?>
 			<option value="<?php echo (int)$dest['id'] ?>"> 
 			<?php echo $dest['nombredest'] ?></option> va a buscar el destinatario por su id, pero va a mostrar su nombre
 			<?php endforeach; ?>
@@ -145,7 +148,7 @@ ob_start();
 			<div class="col-md-7">
 			<select class="form-control" name="egreso-producto" tabindex="2">
 			<option value="">Seleccione Producto</option>
-			<?php  foreach ($all_productos as $prod): ?>
+			<?php foreach ($all_productos as $prod) : ?>
 			<option value="<?php echo (int)$prod['id'] ?>"> 
 			<?php echo $prod['name'] ?></option> va a buscar el producto por su id, pero va a mostrar su nombre
 			<?php endforeach; ?>
@@ -186,7 +189,7 @@ ob_start();
 			<div class="form-group">
 				
 			<div class="col-md-20">
-			<button type="submit" name="add_salida" class="btn btn-success" tabindex="5" <?php $bandera=1; ?>> AGREGAR SALIDA</button>
+			<button type="submit" name="add_salida" class="btn btn-success" tabindex="5" <?php $bandera = 1; ?>> AGREGAR SALIDA</button>
 			</div>
 			</div>
 			</div>
@@ -194,6 +197,6 @@ ob_start();
 		</div>
 			
 	</form>-->
-	
+
 
 <?php include_once('layouts/footer.php'); ?>
--- add_salida2.php
+++ add_salida2.php
@@ -1,275 +1,219 @@
 <?php
-  ob_start();
-  $page_title = 'Nueva Salida';
-  require_once('includes/load.php');
-  // Checkin What level user has permission to view this page
-  page_require_level(2);
-  $all_productos = find_allASC('productos');
-  $all_destinatarios = find_all('destinatarios');
-  $usuario = getIniciales();
+ob_start();
+$page_title = 'Nueva Salida';
+require_once('includes/load.php');
+// Checkin What level user has permission to view this page
+page_require_level(2);
+$all_productos = find_allASC('productos');
+$all_destinatarios = find_all('destinatarios');
+$usuario = getIniciales();
 
 ?>
 
 <?php
 
-	date_default_timezone_set('America/Argentina/Cordoba');
-	$fechaegreso= date("d/m/Y");
+date_default_timezone_set('America/Argentina/Cordoba');
+$fechaegreso = date("d/m/Y");
 
- if (isset($_POST['add_salida'])) 
- { 
+if (isset($_POST['add_salida'])) {
 	$_SESSION['egreso-destinatario'] = $_POST['egreso-destinatario'];
 	$_SESSION['egreso-observacion'] = $_POST['egreso-observacion'];
 	//$e_observacion = $_POST['egreso-observacion'];
- } 
- else
- {
-	 $e_observacion = " ";
- }
- 
- if (isset($_POST['add_salida2']))
-	 $_SESSION['egreso-observacion'] = $_POST['egreso-observacion'];	 
- else
-	 $e_observacion = " ";
+} else {
+	$e_observacion = " ";
+}
 
-	$e_destinatario= $_SESSION['egreso-destinatario'];
-	$e_observacion= $_SESSION['egreso-observacion'];
+if (isset($_POST['add_salida2']))
+	$_SESSION['egreso-observacion'] = $_POST['egreso-observacion'];
+else
+	$e_observacion = " ";
 
- if(isset($_POST['add_salida']))
- {
-   $req_fields = array('egreso-destinatario','egreso-producto', 'egreso-cantidad');
-   
-   validate_fields($req_fields);
-   if(empty($errors))
-   {
-	$e_producto  = remove_junk($db->escape($_POST['egreso-producto']));
-	$e_cantidad  = remove_junk($db->escape($_POST['egreso-cantidad']));
-	//$e_observacion  = remove_junk($db->escape($_POST['egreso-observacion']));	
-	
-	//RESTA CANTIDAD A DB
-	foreach ($all_productos as $prod):
-		if($prod['id'] == $e_producto)
-		{			
-			$servername = "localhost";
-			$username = "root";
-			$password = "";
-			$dbname = "stock_insumos";
-
-			$total = 0;
-			$total = $prod['total'] - $e_cantidad;
-
-			// Create connection
-			$conn = new mysqli($servername, $username, $password, $dbname);
-			// Check connection
-			if ($conn->connect_error) 
-			{
-				die("Connection failed: " . $conn->connect_error);
-			}
-
-			$sql = "UPDATE productos SET total = '{$total}' WHERE id = '{$e_producto}'";
-
-			if ($conn->query($sql) === TRUE) {
-				echo "Record updated successfully";
-			} else {
-				echo "Error updating record: " . $conn->error;
-			}
-
-			$conn->close();
-					
-		}	
-	endforeach;	
-	 
-    $query  = "INSERT INTO salidas (";
-    $query .= "usuario,dateegreso,destinatario,prod,cantidad,observacion";
-    $query .=") VALUES (";
-    $query .=" '{$usuario['iniciales']}',(STR_TO_DATE(REPLACE('{$fechaegreso}','/','.') ,GET_FORMAT(date,'EUR'))), '{$e_destinatario}', '{$e_producto}', '{$e_cantidad}', '{$e_observacion}'";
-    $query .=")";
-
-    if($db->query($query))
-	 {
-       $session->msg('s',"Salida cargada.");
-       redirect('add_salida2.php', false);
-     } 
-	 else 
-	 {
-       $session->msg('d','No se pudo agregar la Salida.');
-       redirect('add_salida.php', false);
-     }
-	} 
-	else
-	{
-		$session->msg("d", $errors);
-		redirect('add_salida.php',false);
-	}  
- } 
- 
-
- if(isset($_POST['add_salida2']))
- {
-	 
-   $req_fields = array('egreso-producto','egreso-cantidad');
-
-   validate_fields($req_fields);
-   if(empty($errors))
-   {
-	$e_producto  = remove_junk($db->escape($_POST['egreso-producto']));
-	$e_cantidad  = remove_junk($db->escape($_POST['egreso-cantidad']));
-	$e_observacion  = remove_junk($db->escape($_POST['egreso-observacion']));
-	
-	//RESTA CANTIDAD A DB
-	foreach ($all_productos as $prod):
-		if($prod['id'] == $e_producto)
-		{			
-			$servername = "localhost";
-			$username = "root";
-			$password = "";
-			$dbname = "stock_insumos";
-
-			$total = 0;
-			$total = $prod['total'] - $e_cantidad;
-
-			// Create connection
-			$conn = new mysqli($servername, $username, $password, $dbname);
-			// Check connection
-			if ($conn->connect_error) 
-			{
-				die("Connection failed: " . $conn->connect_error);
-			}
-
-			$sql = "UPDATE productos SET total = '{$total}' WHERE id = '{$e_producto}'";
-
-			if ($conn->query($sql) === TRUE) {
-				echo "Record updated successfully";
-			} else {
-				echo "Error updating record: " . $conn->error;
-			}
-
-			$conn->close();
-					
-		}	
-	endforeach;	
-	
-    $query  = "INSERT INTO salidas (";
-    $query .= "usuario,dateegreso,destinatario,prod,cantidad,observacion";
-    $query .=") VALUES (";
-    $query .=" '{$usuario['iniciales']}',(STR_TO_DATE(REPLACE('{$fechaegreso}','/','.') ,GET_FORMAT(date,'EUR'))), '{$e_destinatario}', '{$e_producto}', '{$e_cantidad}', '{$e_observacion}'";
-    $query .=")";
+$e_destinatario = $_SESSION['egreso-destinatario'];
+$e_observacion = $_SESSION['egreso-observacion'];
 
 
-    if($db->query($query))
-	 {
-       $session->msg('s',"Salida cargada.");
-       redirect('add_salida2.php', false);
-     } 
-	 else 
-	 {
-       $session->msg('d','No se pudo agregar la Salida.');
-       redirect('add_salida2.php', false);
-     }
-	} 
-	else
-	{
-		$session->msg("d", $errors);
-		redirect('add_salida2.php',false);
+foreach ($all_destinatarios as $dest) :
+	if ($dest['id'] == $e_destinatario) {
+		$destino = $dest['nombredest'];
 	}
-    
- }
+endforeach;
+
+
+if (isset($_POST['add_salida'])) {
+	$req_fields = array('egreso-destinatario', 'egreso-producto', 'egreso-cantidad');
+
+	validate_fields($req_fields);
+	if (empty($errors)) {
+		$e_producto  = remove_junk($db->escape($_POST['egreso-producto']));
+		$e_cantidad  = remove_junk($db->escape($_POST['egreso-cantidad']));
+		//$e_observacion  = remove_junk($db->escape($_POST['egreso-observacion']));	
+
+		//RESTA CANTIDAD A DB
+		foreach ($all_productos as $prod) :
+			if ($prod['id'] == $e_producto) {
+				$servername = "localhost";
+				$username = "root";
+				$password = "";
+				$dbname = "stock_insumos";
+
+				$total = 0;
+				$total = $prod['total'] - $e_cantidad;
+
+				// Create connection
+				$conn = new mysqli($servername, $username, $password, $dbname);
+				// Check connection
+				if ($conn->connect_error) {
+					die("Connection failed: " . $conn->connect_error);
+				}
+
+				$sql = "UPDATE productos SET total = '{$total}' WHERE id = '{$e_producto}'";
+
+				if ($conn->query($sql) === TRUE) {
+					echo "Record updated successfully";
+				} else {
+					echo "Error updating record: " . $conn->error;
+				}
+
+				$conn->close();
+			}
+		endforeach;
+
+		$query  = "INSERT INTO salidas (";
+		$query .= "usuario,dateegreso,destinatario,prod,cantidad,observacion";
+		$query .= ") VALUES (";
+		$query .= " '{$usuario['iniciales']}',(STR_TO_DATE(REPLACE('{$fechaegreso}','/','.') ,GET_FORMAT(date,'EUR'))), '{$e_destinatario}', '{$e_producto}', '{$e_cantidad}', '{$e_observacion}'";
+		$query .= ")";
+
+		if ($db->query($query)) {
+			$session->msg('s', "Salida cargada.");
+			redirect('add_salida2.php', false);
+		} else {
+			$session->msg('d', 'No se pudo agregar la Salida.');
+			redirect('add_salida.php', false);
+		}
+	} else {
+		$session->msg("d", $errors);
+		redirect('add_salida.php', false);
+	}
+}
+
+
+if (isset($_POST['add_salida2'])) {
+
+	$req_fields = array('egreso-producto', 'egreso-cantidad');
+
+	validate_fields($req_fields);
+	if (empty($errors)) {
+		$e_producto  = remove_junk($db->escape($_POST['egreso-producto']));
+		$e_cantidad  = remove_junk($db->escape($_POST['egreso-cantidad']));
+		$e_observacion  = remove_junk($db->escape($_POST['egreso-observacion']));
+
+		//RESTA CANTIDAD A DB
+		foreach ($all_productos as $prod) :
+			if ($prod['id'] == $e_producto) {
+				$servername = "localhost";
+				$username = "root";
+				$password = "";
+				$dbname = "stock_insumos";
+
+				$total = 0;
+				$total = $prod['total'] - $e_cantidad;
+
+				// Create connection
+				$conn = new mysqli($servername, $username, $password, $dbname);
+				// Check connection
+				if ($conn->connect_error) {
+					die("Connection failed: " . $conn->connect_error);
+				}
+
+				$sql = "UPDATE productos SET total = '{$total}' WHERE id = '{$e_producto}'";
+
+				if ($conn->query($sql) === TRUE) {
+					echo "Record updated successfully";
+				} else {
+					echo "Error updating record: " . $conn->error;
+				}
+
+				$conn->close();
+			}
+		endforeach;
+
+		$query  = "INSERT INTO salidas (";
+		$query .= "usuario,dateegreso,destinatario,prod,cantidad,observacion";
+		$query .= ") VALUES (";
+		$query .= " '{$usuario['iniciales']}',(STR_TO_DATE(REPLACE('{$fechaegreso}','/','.') ,GET_FORMAT(date,'EUR'))), '{$e_destinatario}', '{$e_producto}', '{$e_cantidad}', '{$e_observacion}'";
+		$query .= ")";
+
+
+		if ($db->query($query)) {
+			$session->msg('s', "Salida cargada.");
+			redirect('add_salida2.php', false);
+		} else {
+			$session->msg('d', 'No se pudo agregar la Salida.');
+			redirect('add_salida2.php', false);
+		}
+	} else {
+		$session->msg("d", $errors);
+		redirect('add_salida2.php', false);
+	}
+}
 ?>
 <?php include_once('layouts/header.php'); ?>
 
-	<div class="row">
-	<div class="col-md-12">
-    <?php echo display_msg($msg); ?>
-	</div>
-	</div>
+<div class="container-fluid col-9">
+
+	<?php echo display_msg($msg); ?>
+</div>
+<form method="post" action="add_salida2.php" class="clearfix">
+
+	<div class="container-fluid col-8 border border-light border-2 rounded box">
+
+
+		<div class="row estiloHeader">
+			<h2>Destinatario: <?php echo $destino ?> </h2>
+		</div>
 
 		<div class="row">
-		<div class="col-md-10">
-		<div class="panel panel-default">
-        <div class="panel-heading">
-        <strong>
-			<span class="glyphicon glyphicon-th"></span>
-			<span>Agregar Nueva Salida</span>
-        </strong>
-        </div>
-		
-		<div class="panel-body">
-        <div class="col-md-20">
-
-	<form method="post" action="add_salida2.php" class="clearfix">
-
-		<div class="panel-body">
-        <div class="col-md-20">		
-		
-			<div class="col-md-7">
-			<select class="form-control" name="egreso-producto" tabindex="1">
-			<option tabindex="0" value="">Seleccione Producto</option>
-			<?php  foreach ($all_productos as $prod): ?>
-			<option value="<?php echo (int)$prod['id'] ?>"> 
-			<?php echo $prod['name'] ?></option> <!--va a buscar el producto por su id, pero va a mostrar su nombre-->
-			<?php endforeach; ?>
-			</select>
+			<div class="col-md-6">
+				<br>
+				<label for="inputdate" class="form-label">Producto</label>
+				<select class="form-control shadow-sm" name="egreso-producto" tabindex="1">
+					<option value="">Seleccione Producto</option>
+					<?php foreach ($all_productos as $prod) : ?>
+						<option value="<?php echo (int)$prod['id'] ?>">
+							<?php echo $prod['name'] ?></option>
+					<?php endforeach; ?>
+				</select>
 			</div>
-					
-			<div class="col-md-4">
-            <div class="input-group">
-            <span class="input-group-addon">Cantidad</span>
-            <input type="number" class="form-control" autocomplete="off" name="egreso-cantidad" placeholder="" tabindex="2">
-            </div>
-            </div>	
-		
-		</div>
-		</div>		
-
-		
-		<div class="panel-body">
-			<div class="col-md-12">
-			<div class="form-group">
-			<div class="row">
-			
-			<div class="col-md-10">
-			<div class="input-group">
-			<span class="input-group-addon">Observación</span>
-			<textarea name="egreso-observacion"cols="70" rows="5" tabindex="3"></textarea>
-			</div>
-			</div>
-
-			</div>
-			</div>
+			<div class="col-md-3">
+				<br>
+				<label for="inputdate" class="form-label">Cantidad</label>
+				<input type="number" min="1" class="form-control shadow-sm" autocomplete="off" name="egreso-cantidad" placeholder="" tabindex="2">
 			</div>
 		</div>
-		
-			<div class="panel-body">
-			<div class="col-md-12">
-			<div class="form-group">
-				
-			<div class="col-md-20">
-			<button type="submit" name="add_salida2" class="btn btn-success" tabindex="4">AGREGAR SALIDA</button>
-			</div>
-			</div>
-			</div>
-			</div>
-			
-			
-		</div>
-			
-  
-  
-	</form>
-	
-	<div class="panel-body">
-	<div class="col-md-12">
-	<div class="form-group">
-				
-	<div class="col-md-20">
-	<input type="button" value="COMENZAR NUEVA SALIDA" name="B4" class="btn btn-primary" tabindex="5" OnClick=location.href='http://192.168.0.186/stock_insumos/add_salida.php'>
-	</div>
-	</div>
-	</div>
-	</div>
-	
-	
 
+		<br>
+
+		<div class="row">
+			<div class="col-md-6">
+				<label for="inputdate" class="form-label">Observaciones</label>
+				<textarea class="form-control shadow-sm" name="egreso-observacion" rows="3" tabindex="3"></textarea>
+			</div>
+		</div>
+
+		<br><br>
+
+		<div class="row justify-content-end">
+			<div class="col-md-2">
+				<a href="home.php" class="btn btn-dark hoverDeny" tabindex=""><i class="bi bi-x-square"></i> FINALIZAR</a>
+			</div>
+			<br> <br>
+			<div class="col-md-3">
+				<button type="submit" name="add_salida2" class="btn btn-dark hoverAccept" tabindex="4"><i class="bi bi-plus-square"></i> AGREGAR SALIDA</button>
+			</div>
+		</div>
+		<br>
+	</div>
+</form>
 
 <?php include_once('layouts/footer.php'); ?>
-
-
