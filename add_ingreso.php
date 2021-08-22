40d64e07323b054e0f62e7c5569d1017dd05adb9
francoml <francoml92@gmail.com>
2021-08-22 18:47:05 -0300

    .

--- add_ingreso.php
+++ add_ingreso.php
@@ -107,145 +107,15 @@ date_default_timezone_set('America/Argentina/Cordoba');
 
 		<div class="row justify-content-end">
 			<div class="col-md-2">
-				<a href="home.php" class="btn btn-dark hoverDeny" tabindex=""><i class="bi bi-x-square"></i>    CANCELAR</a>
+				<a href="home.php" class="btn btn-dark hoverDeny" tabindex=""><i class="bi bi-x-square"></i> CANCELAR</a>
 			</div>
 			<br> <br>
 			<div class="col-md-3">
-				<button type="submit" name="add_ingreso" class="btn btn-dark hoverAccept" tabindex="9"><i class="bi bi-plus-square"></i>   CARGAR REMITO</button>
+				<button type="submit" name="add_ingreso" class="btn btn-dark hoverAccept" tabindex="9"><i class="bi bi-plus-square"></i> CARGAR REMITO</button>
 			</div>
 		</div>
 		<br>
 	</div>
 </form>
-
-<!--<form method="post" action="add_ingreso2.php" class="clearfix">
-
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<strong>
-				<span class="glyphicon glyphicon-th"></span>
-				<span>Nuevo Ingreso</span>
-			</strong>
-		</div>
-
-		<br>
-
-		<div class="panel-body">
-			<div class="col-12">
-
-				<div class="col-md-4">
-					<div class="input-group">
-						<span class="input-group-addon">Usuario</span>
-						<input readonly="readonly" type="text" class="form-control" style="text-align:center;" value="<?php echo $usuario['iniciales']; ?>">
-					</div>
-				</div>
-
-				<div class="col-md-4">
-					<div class="input-group">
-						<span class="input-group-addon">Fecha de Ingreso</span>
-						<input type="text" class="datepicker form-control" name="ingreso-dateingreso" autocomplete="off" placeholder="" tabindex="2">
-					</div>
-				</div>
-
-				<div class="col-md-4">
-					<div class="input-group">
-						<span class="input-group-addon">Fecha de Carga</span>
-						<input readonly="readonly" type="text" class="form-control" autocomplete="off" style="text-align:center;" value="<?php echo date("d/m/Y"); ?>">
-					</div>
-				</div>
-
-
-			</div>
-		</div>
-
-		<div class="panel-body">
-			<div class="col-12">
-
-				<div class="col-md-6">
-					<select class="form-control" name="ingreso-proveedor" tabindex="4">
-						<option value="">Seleccione Proveedor</option>
-						<?php foreach ($all_proveedores as $prov) : ?>
-							<option value="<?php echo (int)$prov['id'] ?>">
-								<?php echo $prov['nombre'] ?></option>
-							<!--va a buscar el proveedor por su id, pero va a mostrar su nombre
-						<?php endforeach; ?>
-					</select>
-				</div>
-
-				<div class="col-md-6">
-					<div class="input-group">
-						<span class="input-group-addon">Nº Remito</span>
-						<input type="text" class="form-control" name="ingreso-remito" placeholder="" tabindex="5">
-					</div>
-				</div>
-
-			</div>
-		</div>
-
-		<div class="panel-body">
-			<div class="col-12">
-				<div class="col-md-6">
-
-					<select class="form-control" name="ingreso-nombre" tabindex="6">
-						<option value="">Seleccione Producto</option>
-						<?php foreach ($all_productos as $prod) : ?>
-							<option value="<?php echo (int)$prod['id'] ?>">
-								<?php echo $prod['name'] ?></option>
-						<?php endforeach; ?>
-					</select>
-
-				</div>
-			</div>
-		</div>
-
-		<div class="panel-body">
-			<div class="col-12">
-				<div class="col-md-4">
-					<div class="input-group">
-						<span class="input-group-addon">Marca</span>
-						<input type="text" class="form-control" name="ingreso-marca" placeholder="" tabindex="7">
-					</div>
-				</div>
-
-				<div class="col-md-4">
-					<div class="input-group">
-						<span class="input-group-addon">Vencimiento</span>
-						<input type="text" class="datepicker form-control" name="ingreso-datevencimiento" autocomplete="off" placeholder="" tabindex="8">
-					</div>
-				</div>
-
-				<div class="col-md-4">
-					<div class="input-group">
-						<span class="input-group-addon">Cantidad</span>
-						<input type="number" min="1" class="form-control" autocomplete="off" name="ingreso-cantidad" placeholder="" tabindex="9">
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<div class="panel-body">
-			<div class="col-12">
-				<div class="col-md-6">
-					<div class="input-group">
-						<span class="input-group-addon">Observacion</span>
-						<textarea class="form-control" name="ingreso-observacion" rows="5" tabindex="10"></textarea>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<div class="panel-body">
-			<div class="col-12">
-				<div class="col-md-4">
-					<div class="form-group">
-						<button type="submit" name="add_ingreso" class="btn btn-success" tabindex="11">AGREGAR INGRESO</button>
-					</div>
-				</div>
-			</div>
-		</div>
-
-	</div>
-
-</form>-->
 
 <?php include_once('layouts/footer.php'); ?>
